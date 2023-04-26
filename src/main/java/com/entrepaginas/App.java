da14b1f155e7142da9f514e70829cd61bee0a63b
valdir.junior90 <valdir.junior@alunos.ufersa.edu.br>
2023-04-26 13:35:00 -0300

    feat. guardando em CSV

--- src/main/java/com/entrepaginas/App.java
+++ src/main/java/com/entrepaginas/App.java
@@ -15,28 +15,9 @@ import java.io.IOException;
 /**
  * JavaFX App
  */
-public class App extends Application {
-
-    private static Scene scene;
-
-    @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    public static void setRoot(String fxml) throws IOException {
-        scene.setRoot(new AnchorPane());
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
+public class App {
 
     public static void main(String[] args) {
-        launch();
 
 
         String filename = "livros.csv";
